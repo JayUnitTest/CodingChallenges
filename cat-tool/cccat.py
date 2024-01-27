8177e9d091dd65bc58fbdbef4277cc844d521e7f
Jay Singh <jay@Jays-MacBook-Pro.local>
2024-01-27 21:27:21 +0000

    Using Click instead of argparse- steps 0-2 completed and README updated

--- cat-tool/cccat.py
+++ cat-tool/cccat.py
@@ -1,28 +1,29 @@
-import argparse
-import sys
+import click
 
-def main():
+@click.group()
+def cli():
+    pass
 
-    if len(sys.argv) != 2:
-        print("insert a file name")
-        sys.exit(1)
-        
-    filename = sys.argv[1]
+def read_file(fin, num_lines):
+    with open(fin, "r") as f_in:
+            lines = [f_in.readline().strip() for _ in range(num_lines)]
+    return lines
+
+@cli.command()
+@click.argument('fin', type=click.Path(exists=True))
+@click.option('-n', '--num-lines', default=5, type= int, help = "Number of lines to return from file (default is 5)")
+def head(fin, num_lines):
+    print("returning line(s)...\n")
+    lines = read_file(fin, num_lines)
+    for line in lines:
+        click.echo(line.rstrip())
+
+@cli.command()
+@click.argument('fin', nargs=2, type=click.File(mode="r"))
+def cat(fin):
+    for file in fin:
+        click.echo(file.read().strip())
     
-    try:
-        with open(filename, 'r') as fileObj:
-            file_content = fileObj.readlines()
-            print(file_content)
-    except FileNotFoundError:
-        print("File not found:", filename )
-     
-if __name__ == '__main__':
-    main()
 
-
-''' main purpose of sys.argv is to :
-> it is a list of command line arguments
-> len(sys.argv) provides the number of command-line arguments
-> sys.argv[0] is the name of the current python script. 
- so in this case, we are checking if a total of 2 arguments are passed in the command-line - the script and the filename
-'''
+if __name__ == "__main__":
+    cli()
