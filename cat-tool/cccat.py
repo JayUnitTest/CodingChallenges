cf723a6d5aab71ee1ad2be20634de80c81853251
Jay Singh <jay@Jays-MacBook-Pro.local>
2024-01-28 22:48:33 +0000

    changed name-scheme for cat -> cc_cat just to differentiate it better and to include the coding challenge aspect

--- cat-tool/cccat.py
+++ cat-tool/cccat.py
@@ -15,7 +15,7 @@ def read_file(filenames):
 @click.command()
 @click.argument('filenames', nargs=-1, type=click.Path(exists=True))
 @click.option("-n", "number_lines", is_flag=True, help="Number the lines outputted to the console")
-def cat(filenames, number_lines):
+def cc_cat(filenames, number_lines):
     lines = read_file(filenames)
     if number_lines:
         for i, line in enumerate(lines, start=1):
@@ -25,4 +25,4 @@ def cat(filenames, number_lines):
             click.echo(line.strip())
 
 if __name__ == "__main__":
-    cat()
+    cc_cat()
